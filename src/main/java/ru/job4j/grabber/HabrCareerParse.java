9bacb5d3e615dd6a46eb65a076f9a0fdc6cea1a8
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2024-09-06 14:41:42 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,17 +5,26 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
     public static final int COUNT = 5;
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     public static void main(String[] args) throws IOException {
         for (int i = 0; i < COUNT; i++) {
@@ -45,5 +54,26 @@ public class HabrCareerParse {
         }
         Element row = document.select(".vacancy-description__text").first();
         return row.text();
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> rsl = new ArrayList<>();
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            Element dateElement = row.select(".vacancy-card__date").first();
+            Element date = dateElement.child(0);
+            String title = titleElement.text();
+            String linkPost = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String description = retrieveDescription(linkPost);
+            LocalDateTime created = dateTimeParser.parse(date.attr("dateTime"));
+            Post post = new Post(title, linkPost, description, created);
+            rsl.add(post);
+        });
+        return rsl;
     }
 }
